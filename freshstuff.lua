9dad21b2798bc2ab07ce18754aaed862ad4369b1
bastyaelvtars <bastyaelvtars@gmail.com>
2008-08-03 20:03:14 +0000

    fixed: forgot to commit the changed freshstuff.lua

--- freshstuff.lua
+++ freshstuff.lua
@@ -1,21 +1,20 @@
---[[ 
+--[[
 FreshStuff3 v5
 This is the common script that gets loaded by host apps, then takes care of everything else :-D
 Characteristics (well, proposed - no, they are almost real as of end-feb 2007): modular and portable among host programs
 Distributed under the terms of the Common Development and Distribution License (CDDL) Version 1.0. See docs/license.txt for details.
 ]]
 AllStuff,NewestStuff,Engine,Bot,Commands,Levels = {},{},{},{},{},{}
-Bot.version="FreshStuff3 5.0"
+Bot.version="FreshStuff3 5.5 alpha 1"
 ModulesLoaded = {}
 
 do -- detect the host app
 -- This is done by detecting global tables that are specific to the host app.
 local Host=
   {
-    ["frmHub"]= {func = "GetPtokaXLocation", param = "frmHub", path = "scripts/freshstuff/?.lua", mod = "ptokax"},
 --     ["DC"]="bcdc",
 --     ["VH"]="verli",
-    ["Core"] = {func = "GetPtokaXPath", path = "scripts/freshstuff/?.lua", mod = "ptokaxnew"},
+    ["Core"] = {func = "GetPtokaXPath", path = "scripts/freshstuff/?.lua", mod = "ptokax"},
   }
 
 local c
@@ -39,25 +38,13 @@ require "kernel"
 
 local hostloader =
   {
-    ["ptokax"] = -- This is for old PtokaX
+    ["ptokax"] =
       function()
-        package.cpath = frmHub:GetPtokaXLocation().."/scripts/freshstuff/lib/?.dll" -- Set the path for C libs.
-        require "pxlfs" 
-        package.path = frmHub:GetPtokaXLocation().."/scripts/freshstuff/components/?.lua" -- Set the path for Lua libs.
-        for entry in lfs.dir(  frmHub:GetPtokaXLocation().."/scripts/freshstuff/components" ) do -- open the components directory
-          local filename, ext = entry:match("([^%.]+)%.(%w%w%w)") -- search for Lua files
-          if ext == "lua" then
-            require (filename) -- and load them
-          end
-        end
-      end,
-    ["ptokaxnew"] = 
-      function()
-        package.cpath=Core.GetPtokaXPath().."scripts/freshstuff/libnew/?.dll"
+        package.cpath = Core.GetPtokaXPath().."scripts/freshstuff/libnew/?.dll"
         require "pxlfs"
-        package.path=Core.GetPtokaXPath().."scripts/freshstuff/components/?.lua"
+        package.path = Core.GetPtokaXPath().."scripts/freshstuff/components/?.lua"
         for entry in lfs.dir( Core.GetPtokaXPath().."scripts/freshstuff/components" ) do
-          local filename,ext=entry:match("([^%.]+)%.lua$")
+          local filename, ext = entry:match("([^%.]+)%.lua$")
           if filename then require (filename) end
         end
       end,
